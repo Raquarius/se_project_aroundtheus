8081c3294fcda114708a9f043b91c22eed553abf
Ricardo Cruz <rctravels96@gmail.com>
2024-03-11 16:42:00 -0500

    JS added(3)

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const initialCards = [
   {
-    title: "Yosemite Valley",
+    name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {

b19f52839604cca1989c712ca98f5b9658257ceb
Ricardo Cruz <rctravels96@gmail.com>
2024-03-21 00:01:31 -0500

    Profile edit rendered

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,23 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+const profileEditBtn = document.querySelector("#profile-edit-btn");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseBtn = document.querySelector(".modal__close");
+const profileName = document.querySelector(".profile__name");
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileOccupation = document.querySelector(".profile__occupation");
+const profileOccupationInput = document.querySelector(
+  "#profile-occupation-input"
+);
+
+profileEditBtn.addEventListener("click", function () {
+  profileNameInput.value = profileName.textContent;
+  profileEditModal.classList.add("modal_opened");
+  profileOccupationInput.value = profileOccupation.textContent;
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseBtn.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_opened");
+});

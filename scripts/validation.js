4bda6e02d413ab186d8681e507f258f661afedfd
Ricardo Cruz <rctravels96@gmail.com>
2024-05-13 13:38:07 -0500

    add validation.js

---
+++ scripts/validation.js
@@ -0,0 +1,15 @@
+// function enableValidation(config) {
+//   const formElements = array.from(document.querySelectorAll("modal__form"));
+//   console.log(formElements.map);
+// }
+
+// const config = {
+//   formSelector: ".modal__form",
+//   inputSelector: ".modal__form-input",
+//   submitButtonSelector: ".modal__button",
+//   inactiveButtonClass: "modal__button_disabled",
+//   inputErrorClass: "modal__input_type_error",
+//   errorClass: "modal__error_visible",
+// };
+
+// enableValidation(config);

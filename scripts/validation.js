f1a6b44069dbaec6e94269e27cf4713107315923
Ricardo Cruz <rctravels96@gmail.com>
2024-05-14 21:47:48 -0500

    create functions and event listers for error validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,15 +1,47 @@
-// function enableValidation(config) {
-//   const formElements = array.from(document.querySelectorAll("modal__form"));
-//   console.log(formElements.map);
-// }
+function showInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classlist.add(inputErrorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+  errorMessageElement.classlist.add(errorClass);
+}
+function checkInputValidity(formElement, inputElement, options) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, options);
+  } else {
+    hideInputError(formElement, inputElement, options);
+  }
+}
 
-// const config = {
-//   formSelector: ".modal__form",
-//   inputSelector: ".modal__form-input",
-//   submitButtonSelector: ".modal__button",
-//   inactiveButtonClass: "modal__button_disabled",
-//   inputErrorClass: "modal__input_type_error",
-//   errorClass: "modal__error_visible",
-// };
+function setEventListener(formElement, options) {
+  const { inputSelector } = options;
+  const inputElements = [...formElement.querySelectorAll(inputSelector)];
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", (evt) => {});
+  });
+}
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListener(formElement, options);
+  });
+}
 
-// enableValidation(config);
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__form-input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
